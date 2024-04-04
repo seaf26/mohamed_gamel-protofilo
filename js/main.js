93ccd319bc27e851693ac4e68689b4fce9a006e9
seaf26 <seafmohamedgamel@gmail.com>
2024-04-05 01:45:57 +0200

    finshied media query

--- js/main.js
+++ js/main.js
@@ -1,20 +1,62 @@
+let navbar = document.querySelector(".navbar");
+let menu = document.querySelector(".menu");
+let links = document.getElementById("hidelinks");
+let navlinks = document.querySelector(".links");
+let button = document.querySelector(".navbar .buttton");
 
+console.log(navbar);
+console.log(menu);
+console.log(links);
+console.log(navlinks);
+console.log(button);
 
-let buttons=document.querySelectorAll(".projects .project")
-let i=0;
-buttons.forEach(button => {
-    i++
-            if(i %2==0){
-            button.style.flexDirection='row-reverse'
-            
-        }
-    })
+// menu.addEventListener("click", () => {
+//     navbar.classList.toggle("show");
+// });
 
-    document.getElementById("download").addEventListener("click", function() {
-        var link = document.createElement('a');
-      link.setAttribute('href', 'https://drive.google.com/file/d/1ul_ZZmWzsCaYF4r7OHahL9Sc8J5z4u2s/view?usp=sharing');
-      link.setAttribute('download', 'Seaf_GamelCV.pdf');
-      link.setAttribute('rel', 'noopener');
-      link.click();
-      });
-      console.log("download")
+let screenWidth = window.innerWidth;
+window.addEventListener("resize", () => {
+  screenWidth = window.innerWidth;
+  if (screenWidth > 995) {
+    menu.style.display = "none";
+    button.style.display = "flex";
+
+    navlinks.style.display = "flex";
+  } else {
+    navlinks.style.display = "none";
+    button.style.display = "none";
+    menu.style.display = "flex";
+  }
+});
+
+let buttons = document.querySelectorAll(".projects .project");
+let i = 0;
+buttons.forEach((button) => {
+  i++;
+  if (i % 2 == 0) {
+    button.style.flexDirection = "row-reverse";
+  }
+});
+
+document.getElementById("download").addEventListener("click", function () {
+  var link = document.createElement("a");
+  link.setAttribute(
+    "href",
+    "https://drive.google.com/file/d/1FbZX7iXv7ctVi8zewmVm1Lz2ZxaVd20W/view?usp=drive_link"
+  );
+  link.setAttribute("download", "Seaf_GamelCV.pdf");
+  link.setAttribute("rel", "noopener");
+  link.click();
+});
+
+document.getElementById("joe").addEventListener("click", function () {
+    var linkk = document.createElement("a");
+    linkk.setAttribute(
+      "href",
+      "https://drive.google.com/file/d/1FbZX7iXv7ctVi8zewmVm1Lz2ZxaVd20W/view?usp=drive_link"
+    );
+    linkk.setAttribute("download", "Seaf_GamelCV.pdf");
+    linkk.setAttribute("rel", "noopener");
+    linkk.click();
+  });
+
